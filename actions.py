56ef555c984d6bca17921aa4527ea82207dbdbe1
Shabiha Begum <shabihajaleel@gmail.com>
2025-06-14 12:36:39 +0530

    updated actions.py

--- actions.py
+++ actions.py
@@ -13,12 +13,26 @@ driver.maximize_window()
 
 
 # range selection
+# actions = ActionChains(driver)
+# slider = driver.find_element(By.ID, "sliderContainer")
+# actions.click_and_hold(slider).move_by_offset(-slider.size['width'] // 2, 0).release().perform()
+# time.sleep(2)
+# actions.click_and_hold(slider).move_by_offset(51, 0).release().perform()
+# # driver.execute_script("arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('change'));", slider, "51")
+
+# value = driver.find_element(By.ID, "sliderValue").get_attribute("value")
+# print(value)
+# print("success #2")
+
+
+slider = driver.find_element(By.CLASS_NAME, "range-slider")
+slider_width = slider.size['width']
+target_value = 51
+# Convert the desired value to a pixel offset (approximate)
+offset = int((target_value / 100) * slider_width) - (slider_width // 2)
+
 actions = ActionChains(driver)
-slider = driver.find_element(By.ID, "sliderContainer")
-actions.click_and_hold(slider).move_by_offset(-slider.size['width'] // 2, 0).release().perform()
-time.sleep(2)
-actions.click_and_hold(slider).move_by_offset(51, 0).release().perform()
-# driver.execute_script("arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('change'));", slider, "51")
+actions.click_and_hold(slider).move_by_offset(offset, 0).release().perform()
 
 value = driver.find_element(By.ID, "sliderValue").get_attribute("value")
 print(value)
